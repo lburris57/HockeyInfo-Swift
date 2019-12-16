da311511902cdd107a4f176e144af7e6c0a338e2
Larry Burris <lburris@mac.com>
2019-12-16 00:25:16 -0500

    Initial version

---
+++ HockeyInfo-Swift/View Controllers/WelcomeScreenViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  HockeyInfo-Swift
+//
+//  Created by Larry Burris on 12/15/19.
+//  Copyright © 2019 Larry Burris. All rights reserved.
+//
+import UIKit
+
+class WelcomeScreenViewController: UIViewController
+{
+    @IBOutlet weak var HockeyInfoLabel: UILabel!
+    @IBOutlet weak var poweredByLabel: UILabel!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        displayHockeyInfoScrollingText()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.25)
+        {
+            self.displayPoweredByScrollingText()
+        }
+    }
+    
+    func displayHockeyInfoScrollingText()
+    {
+        HockeyInfoLabel.text = "Hockey Info"
+    }
+    
+    func displayPoweredByScrollingText()
+    {
+        poweredByLabel.text = ""
+        poweredByLabel.text = "Powered by My Sports Feeds"
+    }
+}
+
